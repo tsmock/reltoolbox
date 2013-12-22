f5d3c4e8aa309b4f986e800fabf1d663341dc0e7
don-vip <vincent.privat@gmail.com>
2013-12-22 11:27:11 +0000

    [josm-plugins] global replacement of `Main.map.mapview.getEditLayer()` by `Main.main.getEditLayer()` (avoids NPEs)

--- src/relcontext/actions/DownloadChosenRelationAction.java
+++ src/relcontext/actions/DownloadChosenRelationAction.java
@@ -1,55 +1,62 @@
 package relcontext.actions;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
-
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
 import javax.swing.AbstractAction;
-
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.actions.downloadtasks.DownloadReferrersTask;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.gui.dialogs.relation.DownloadRelationMemberTask;
 import org.openstreetmap.josm.gui.dialogs.relation.DownloadRelationTask;
 import org.openstreetmap.josm.tools.ImageProvider;
-
 import relcontext.ChosenRelation;
 import relcontext.ChosenRelationListener;
 
 /**
- * Downloads parent relations for this relation and all parent objects for its members.
+ * Downloads or updates chosen relation members, depending on completeness.
  * 
  * @author Zverik
  */
-public class DownloadParentsAction extends AbstractAction implements ChosenRelationListener {
+public class DownloadChosenRelationAction extends AbstractAction implements ChosenRelationListener {
     private ChosenRelation rel;
 
-    public DownloadParentsAction( ChosenRelation rel ) {
-        super(tr("Download referrers"));
-        putValue(SMALL_ICON, ImageProvider.get("downloadreferrers"));
-        putValue(SHORT_DESCRIPTION, tr("Download referrers for the chosen relation and its members."));
+    public DownloadChosenRelationAction( ChosenRelation rel ) {
+        super();
+//        putValue(NAME, "D");
+        putValue(SMALL_ICON, ImageProvider.get("relcontext", "download"));
+        putValue(SHORT_DESCRIPTION, tr("Download incomplete members for the chosen relation"));
         this.rel = rel;
         rel.addChosenRelationListener(this);
-        setEnabled(rel.get() != null && Main.main.getEditLayer() != null);
+        setEnabled(false);
     }
 
     public void actionPerformed( ActionEvent e ) {
         Relation relation = rel.get();
-        if( relation == null ) return;
-        List<OsmPrimitive> objects = new ArrayList<OsmPrimitive>();
-        objects.add(relation);
-        objects.addAll(relation.getMemberPrimitives());
-        Main.worker.submit(new DownloadReferrersTask(Main.main.getEditLayer(), objects));
+        if( relation == null || relation.isNew() ) return;
+        int total = relation.getMembersCount();
+        int incomplete = relation.getIncompleteMembers().size();
+//        if( incomplete <= 5 || (incomplete <= 10 && incomplete * 3 < total) )
+        if( incomplete <= 10 && incomplete * 3 < total )
+            downloadIncomplete(relation);
+        else
+            downloadMembers(relation);
     }
 
     public void chosenRelationChanged( Relation oldRelation, Relation newRelation ) {
-        setEnabled(newRelation != null && Main.main.getEditLayer() != null);
+        boolean incomplete = false;
+        if( newRelation != null ) {
+            for( RelationMember m : newRelation.getMembers()) {
+                if( m.getMember().isIncomplete() ) {
+                    incomplete = true;
+                    break;
+                }
+            }
+        }
+        setEnabled(newRelation != null && incomplete);
     }
 
     protected void downloadMembers( Relation rel ) {
